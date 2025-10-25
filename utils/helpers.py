8812599f6fab397e35f7eed3a6acb7c3b5be2c28
maxiflores9902 <maxi.flores9902@gmail.com>
2025-10-25 00:10:38 -0300

    feat: refactor structure - create dir utils

---
+++ utils/helpers.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def iniciar_driver():
+    """Inicia el driver de Chrome con opciones basicas"""
+    options = Options()
+    options.add_argument("--start-maximized")
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+def esperar_elemento(driver, by, locator, timeout=10):
+    """Espera de un elemento visible"""
+    wait = WebDriverWait(driver, timeout)
+    return wait.until(EC.visibility_of_element_located((by, locator)))
