67351f10d3a29244ce83329e91092f7170d158cd
maxiflores9902 <maxi.flores9902@gmail.com>
2025-10-25 00:18:44 -0300

    fix: flake8

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -6,11 +6,13 @@ URL = "https://www.saucedemo.com/"
 USERNAME = "standard_user"
 PASSWORD = "secret_sauce"
 
+
 @pytest.fixture
 def driver():
     driver = iniciar_driver()
     yield driver
     driver.quit()
+
 
 def test_login_exitoso(driver):
     driver.get(URL)
@@ -22,6 +24,7 @@ def test_login_exitoso(driver):
     assert "/inventory.html" in driver.current_url
     titulo = esperar_elemento(driver, By.CLASS_NAME, "title").text
     assert "Products" in titulo
+
 
 def test_catalogo_visible(driver):
     driver.get(URL)
@@ -35,9 +38,12 @@ def test_catalogo_visible(driver):
     productos = driver.find_elements(By.CLASS_NAME, "inventory_item")
     assert len(productos) > 0
 
-    nombre = productos[0].find_element(By.CLASS_NAME, "inventory_item_name").text
-    precio = productos[0].find_element(By.CLASS_NAME, "inventory_item_price").text
+    nombre = productos[0].find_element(
+        By.CLASS_NAME, "inventory_item_name").text
+    precio = productos[0].find_element(
+        By.CLASS_NAME, "inventory_item_price").text
     print(f"Primer producto: {nombre} - {precio}")
+
 
 def test_agregar_producto_al_carrito(driver):
     driver.get(URL)
@@ -45,12 +51,15 @@ def test_agregar_producto_al_carrito(driver):
     esperar_elemento(driver, By.ID, "password").send_keys(PASSWORD)
     esperar_elemento(driver, By.ID, "login-button").click()
 
-    primer_producto_btn = esperar_elemento(driver, By.XPATH, "(//button[contains(text(),'Add to cart')])[1]")
+    primer_producto_btn = esperar_elemento(
+        driver, By.XPATH, "(//button[contains(text(),'Add to cart')])[1]")
     primer_producto_btn.click()
 
-    contador = esperar_elemento(driver, By.CLASS_NAME, "shopping_cart_badge").text
+    contador = esperar_elemento(
+        driver, By.CLASS_NAME, "shopping_cart_badge").text
     assert contador == "1"
 
     esperar_elemento(driver, By.CLASS_NAME, "shopping_cart_link").click()
-    producto_en_carrito = esperar_elemento(driver, By.CLASS_NAME, "inventory_item_name").text
+    producto_en_carrito = esperar_elemento(
+        driver, By.CLASS_NAME, "inventory_item_name").text
     assert producto_en_carrito != ""
